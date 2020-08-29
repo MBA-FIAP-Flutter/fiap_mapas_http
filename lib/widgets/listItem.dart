9e71c0d5ed8ec8f810beb5f64a3ec623c5bb387e
Ricardo Ogliari <ricardo.ogliari@gok.digital>
2020-08-29 14:47:38 -0300

    começo da state do ListTrucks

--- lib/widgets/listItem.dart
+++ lib/widgets/listItem.dart
@@ -10,3 +10,34 @@ class ListItemVehicle extends StatefulWidget {
   @override
   _ListItemVehicleState createState() => _ListItemVehicleState();
 }
+
+class _ListItemVehicleState extends State<ListItemVehicle> {
+
+  TextStyle style15dp = TextStyle(fontSize: 15);
+  TextStyle style27dp = TextStyle(fontSize: 27);
+  Geolocator.Placemark placemark;
+
+  @override
+  void initState() {
+    super.initState();
+    getAddress();
+  }
+
+  getAddress() async {
+    List<Geolocator.Placemark> listPlacemarks =
+    await Geolocator.Geolocator()
+        .placemarkFromCoordinates(
+          widget.position.lat,
+          widget.position.lng);
+
+    //for class pattern
+    setState(() {
+      placemark = listPlacemarks[0];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) { }
+
+}
+
