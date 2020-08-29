8a2768caeee39111dbec3447a2b8767d60ccd79c
Ricardo Ogliari <ricardo.ogliari@gok.digital>
2020-08-29 14:26:51 -0300

    alteração na main.dart e escopo inicial das telas

---
+++ lib/screens/listTrucks.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ListTrucks extends StatefulWidget {
+  @override
+  _ListTrucksState createState() => _ListTrucksState();
+}
+
+class _ListTrucksState extends State<ListTrucks> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
