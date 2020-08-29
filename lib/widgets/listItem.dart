9e324859240422c79f4f0bea7e6dcd22a2314f82
Ricardo Ogliari <ricardo.ogliari@gok.digital>
2020-08-29 14:59:56 -0300

    fim das alterações da ListTrucks

--- lib/widgets/listItem.dart
+++ lib/widgets/listItem.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_fiap_aula_http_mapas/model/position.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:geolocator/geolocator.dart' as Geolocator;
 
 class ListItemVehicle extends StatefulWidget {
@@ -37,7 +38,64 @@ class _ListItemVehicleState extends State<ListItemVehicle> {
   }
 
   @override
-  Widget build(BuildContext context) { }
+  Widget build(BuildContext context) {
+    return Slidable(
+      actionPane: SlidableDrawerActionPane(),
+      actionExtentRatio: 0.25,
+      child: InkWell(
+        child: Padding(
+          padding: EdgeInsets.all(12.0),
+          child: Row(
+            children: [
+              Expanded(//expanded: obriga a widget a expandir até o espaço possível
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                        widget.position.veiculo_placa.trim(),
+                        style: style27dp
+                    ),
+                    Text(
+                        widget.position.condutor_nome == null ?
+                        "-" : widget.position.condutor_nome,
+                        style: style15dp
+                    ),
+                    Text(
+                      placemark == null ?
+                      "Buscando endereço..." : getFormattedAddress(),
+                      style: style15dp,
+                      softWrap: false,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+              ),
+              Icon(Icons.keyboard_arrow_right)
+            ],
+          ),
+        ),
+        onTap: (){
+          Navigator.pushNamed(context, "/mapa", arguments: widget.position);
+        },
+      ),
+      actions: <Widget>[],//da esquerda pra direita
+      secondaryActions: <Widget>[
+        IconSlideAction(
+          caption: 'Comandos',
+          color: Colors.lightBlue,
+          foregroundColor: Colors.white,
+          icon: Icons.archive,
+          onTap: () {},
+        ),
+      ],//da direita pra esquerda
+    );
+  }
+
+  String getFormattedAddress(){
+    return "${placemark.subAdministrativeArea}, ${placemark.administrativeArea}";
+  }
+
+
 
 }
 
